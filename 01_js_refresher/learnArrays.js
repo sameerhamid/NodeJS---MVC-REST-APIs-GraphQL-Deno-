c52a4abdc794b61593b6d6befbae22ba0b7219b0
sameerhamid <sameerhamid213@gmail.com>
2024-10-12 10:44:27 +0530

    add reset operator

--- 01_js_refresher/learnArrays.js
+++ 01_js_refresher/learnArrays.js
@@ -12,3 +12,9 @@ for (let hobby of hobbies) {
 // const coppiedHobbies = hobbies.slice()
 const coppiedHobbies = [...hobbies, "Cooking"];
 console.log(coppiedHobbies);
+
+function toArray(...args) {
+  return args;
+}
+
+console.log(toArray(1, 2, 3, 4));
