70bc902b5a2ba4ee8ff99f378d03cf733dca73cd
sameerhamid <sameerhamid213@gmail.com>
2024-10-12 12:30:56 +0530

    add send respose as form and form action will rederict to message

--- 03_understanding-basics-nodejs/app.js
+++ 03_understanding-basics-nodejs/app.js
@@ -8,30 +8,75 @@ const http = require("http");
 //   console.log(req);
 // });
 
+// const server = http.createServer((req, res) => {
+//   console.log("url>>>>>", req.url);
+//   console.log("method >>>", req.method);
+//   console.log("headers >>>>>", req.headers);
+
+//   res.setHeader("Content-Type", "text/html");
+
+//   res.write(`
+//         <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h2>Hello from my node js server</h2>
+// </body>
+// </html>
+//         `);
+//   // end stops wring
+//   res.end();
+// });
+
 const server = http.createServer((req, res) => {
-  console.log("url>>>>>", req.url);
-  console.log("method >>>", req.method);
-  console.log("headers >>>>>", req.headers);
+  const { url } = req;
+  if (url === "/") {
+    res.write(`
 
-  res.setHeader("Content-Type", "text/html");
-
-  res.write(`
         <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h2>Hello from my node js server</h2>
-</body>
-</html>
-        `);
-  // end stops wring
-  res.end();
-});
 
+<body>
+    <form method="POST" action="/message">
+        <div>
+            <input type="text" name="message" />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+</html>
+            `);
+
+    res.end();
+  }
+
+  //   res.write(`
+  //         <!DOCTYPE html>
+  // <html lang="en">
+  // <head>
+  //     <meta charset="UTF-8">
+  //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //     <title>Document</title>
+  // </head>
+  // <body>
+  //     <h2>Hello from my node js server</h2>
+  // </body>
+  // </html>
+  //         `);
+  //   // end stops wring
+  //   res.end();
+});
 /*listen starts a process for node 
 js will not emidiatly exit our script
  but node js will instead keep this running to listen for incomming requests
