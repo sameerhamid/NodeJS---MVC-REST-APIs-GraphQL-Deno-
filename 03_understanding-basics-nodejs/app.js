bd64bde1778f98699eb2eebcd8962770f0a3fe13
sameerhamid <sameerhamid213@gmail.com>
2024-10-12 12:19:11 +0530

    add setHeaders and send response as html document

--- 03_understanding-basics-nodejs/app.js
+++ 03_understanding-basics-nodejs/app.js
@@ -12,6 +12,24 @@ const server = http.createServer((req, res) => {
   console.log("url>>>>>", req.url);
   console.log("method >>>", req.method);
   console.log("headers >>>>>", req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.write(`
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello from my node js server</h2>
+</body>
+</html>
+        `);
+  // end stops wring
+  res.end();
 });
 
 /*listen starts a process for node 
