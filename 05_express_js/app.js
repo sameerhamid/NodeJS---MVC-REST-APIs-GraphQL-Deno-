254a24a76a1054926f7517a55c79a4a8edddc13f
sameerhamid <sameerhamid213@gmail.com>
2024-10-13 18:23:17 +0530

    add page not found route

--- 05_express_js/app.js
+++ 05_express_js/app.js
@@ -52,8 +52,14 @@ app.use(shopRoutes);
 // app.use("/", (req, res, next) => {
 //   res.send(`<h1>Hello from express</h1>`);
 // });
+
+// will use to catch for other routes
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>Page not found</h1>`);
+});
 // const server = http.createServer(app);
 
 // server.listen(3000);
+
 // --------- it does both things creats server and listens
 app.listen(3000);
