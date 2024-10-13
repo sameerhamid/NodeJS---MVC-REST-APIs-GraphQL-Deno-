c49facf8d2d242e179589e8d467372a0a6f0c73b
sameerhamid <sameerhamid213@gmail.com>
2024-10-13 17:10:27 +0530

    learn about middleware and use of next callback function

--- 05_express_js/app.js
+++ 05_express_js/app.js
@@ -4,6 +4,14 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next(); // allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("in the second middleware");
+});
 const server = http.createServer(app);
 
 server.listen(3000);
