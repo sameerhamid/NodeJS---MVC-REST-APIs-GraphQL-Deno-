32e6d78945ef7dd29126cf48f1aefb363d76edd6
sameerhamid <sameerhamid213@gmail.com>
2024-10-13 17:46:51 +0530

    learn how to create multiple routes

--- 05_express_js/app.js
+++ 05_express_js/app.js
@@ -4,17 +4,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
+app.use("/", (req, res, next) => {
+  res.send("This always runs!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("in the add product");
+  res.send(`<h1>Add produect</h1>`);
   next(); // allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("in the second middleware");
   res.send(`<h1>Hello from express</h1>`);
 });
 // const server = http.createServer(app);
 
 // server.listen(3000);
-
+// --------- it does both things creats server and listens
 app.listen(3000);
