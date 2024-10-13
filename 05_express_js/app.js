901f97493f0474452481247a77c5709c93387903
sameerhamid <sameerhamid213@gmail.com>
2024-10-13 18:04:29 +0530

    learn how to get data from request

--- 05_express_js/app.js
+++ 05_express_js/app.js
@@ -3,20 +3,41 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", (req, res, next) => {
-  res.send("This always runs!");
-  next();
-});
+// app.use("/", (req, res, next) => {
+//   res.send("This always runs!");
+//   next();
+// });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("in the add product");
-  res.send(`<h1>Add produect</h1>`);
+  res.send(`
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="/product" method="POST">
+        <input type="text" name="title" />
+        <button type="submit">Add product</button>
+    </form>
+</body>
+
+</html>
+    `);
   next(); // allows the request to continue to the next middleware in line
 });
 
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 app.use("/", (req, res, next) => {
-  console.log("in the second middleware");
   res.send(`<h1>Hello from express</h1>`);
 });
 // const server = http.createServer(app);
