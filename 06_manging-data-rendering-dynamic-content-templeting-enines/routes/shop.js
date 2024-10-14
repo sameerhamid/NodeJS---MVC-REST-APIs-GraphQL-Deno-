0cfa261f3e647e2f4059f469f3632cf8a4d143ae
sameerhamid <sameerhamid213@gmail.com>
2024-10-14 21:03:41 +0530

    add products arry in admin and push data into that array

--- 06_manging-data-rendering-dynamic-content-templeting-enines/routes/shop.js
+++ 06_manging-data-rendering-dynamic-content-templeting-enines/routes/shop.js
@@ -1,13 +1,15 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
