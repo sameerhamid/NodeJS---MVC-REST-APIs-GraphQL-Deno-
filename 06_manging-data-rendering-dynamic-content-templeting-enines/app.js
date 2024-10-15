8bd06f02f52db3aff732a86304b833550b9d7250
sameerhamid <sameerhamid213@gmail.com>
2024-10-15 22:04:54 +0530

    create for add-product and 404 page pug files and render these pug files

--- 06_manging-data-rendering-dynamic-content-templeting-enines/app.js
+++ 06_manging-data-rendering-dynamic-content-templeting-enines/app.js
@@ -18,8 +18,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.router);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
+// app.use((req, res, next) => {
+//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+// });
 
+app.use((req, res, next) => {
+  res.statu(404).render("404");
+});
 app.listen(3000);
--- 06_manging-data-rendering-dynamic-content-templeting-enines/routes/admin.js
+++ 06_manging-data-rendering-dynamic-content-templeting-enines/routes/admin.js
@@ -8,8 +8,13 @@ const router = express.Router();
 
 const products = [];
 // /admin/add-product => GET
+// router.get("/add-product", (req, res, next) => {
+//   res.sendFile(path.join(rootDir, "views", "add-product.html"));
+// });
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { docTitle: "Add Product" });
 });
 
 // /admin/add-product => POST
