971cbd8fc6e974c5775d03ed336b882bd75ce84d
sameerhamid <sameerhamid213@gmail.com>
2024-10-17 20:38:45 +0530

    add error controller

--- 08_model_view_controller/app.js
+++ 08_model_view_controller/app.js
@@ -11,14 +11,14 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+const { get404 } = require("./controllers/error.controller");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(get404);
 
 app.listen(3000);
