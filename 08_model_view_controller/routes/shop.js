e1cb79b8e7ffbbe9faea9df0de9741f66732103c
sameerhamid <sameerhamid213@gmail.com>
2024-10-17 20:30:04 +0530

    add porducts controller

--- 08_model_view_controller/routes/shop.js
+++ 08_model_view_controller/routes/shop.js
@@ -1,22 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
+
+const { getProducts } = require("../controllers/products.controller");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get("/", getProducts);
 
 module.exports = router;
