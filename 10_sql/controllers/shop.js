aec459f6d02375d6eeec0996a54e07ec960b609c
sameerhamid <sameerhamid213@gmail.com>
2024-10-20 08:55:15 +0530

    add project setup to learn sql

---
+++ 10_sql/controllers/shop.js
@@ -0,0 +1,87 @@
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const { productId } = req.params;
+  Product.findById(productId, (product) => {
+    res.render("shop/product-detail", {
+      product,
+      pageTitle: "Product detail",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  Cart.getProducts((cartProducts) => {
+    const cart = [];
+    Product.fetchAll((products) => {
+      for (prod of products) {
+        const cartProductDAta = cartProducts.products.find(
+          (p) => p.id === prod.id
+        );
+        if (cartProductDAta) {
+          cart.push({ productData: prod, qty: cartProductDAta.qty });
+        }
+      }
+
+      console.log("cartProductDAta>>>", cart);
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cart,
+      });
+    });
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
+
+exports.postCartDelete = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
---
+++ 10_sql/models/product.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+const path = require("path");
+const Cart = require("./cart");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      // check for if id is already present then update the product
+      if (this.id) {
+        const existingIndex = products.findIndex((p) => p.id === this.id);
+
+        const updatedProducts = [...products];
+
+        updatedProducts[existingIndex] = this;
+
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = `p${products.length + 1}`;
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const prd = products.filter((prod) => prod.id === id);
+      const updatedProducts = products.filter((p) => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, prd.price);
+        }
+        console.log(err);
+      });
+    });
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((product) => {
+      const prd = product.find((p) => p.id === id);
+      cb(prd);
+    });
+  }
+};
