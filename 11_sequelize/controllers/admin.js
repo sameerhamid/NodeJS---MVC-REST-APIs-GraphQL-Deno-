bea18f7c29119145c1bebb6b3e8b27d3ed557924
sameerhamid <sameerhamid213@gmail.com>
2024-10-26 07:28:50 +0530

    get product based on the user

--- 11_sequelize/controllers/admin.js
+++ 11_sequelize/controllers/admin.js
@@ -146,7 +146,8 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
