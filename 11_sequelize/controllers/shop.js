bb2ceef2c818b3852fa313bda6295dd8845dedcb
sameerhamid <sameerhamid213@gmail.com>
2024-10-26 08:47:24 +0530

    add logic for already in cart increase only quantity

--- 11_sequelize/controllers/shop.js
+++ 11_sequelize/controllers/shop.js
@@ -94,33 +94,30 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
       fetchedCart = cart;
-      return cart
-        .getProducts({ where: { id: prodId } })
-        .then((products) => {
-          let product;
-          if (products.length > 0) {
-            product = products[0];
-          }
-          let newQuantity = 1;
-          if (product) {
-          }
+      return cart.getProducts({ where: { id: prodId } }).then((products) => {
+        let product;
+        if (products.length > 0) {
+          product = products[0];
+        }
 
-          return Product.findByPk(prodId)
-            .then((product) => {
-              fetchedCart.addProduct(product, {
-                through: { quantity: newQuantity },
-              });
-            })
-            .catch((errr) => {
-              console.log("Error finding product: ", errr);
-              return res.redirect("/");
-            });
-        })
-        .catch((err) => {});
+        if (product) {
+          const oldQuantity = product.cartItem.quantity;
+          newQuantity = oldQuantity + 1;
+          return product;
+        }
+
+        return Product.findByPk(prodId);
+      });
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
       res.redirect("/cart");
