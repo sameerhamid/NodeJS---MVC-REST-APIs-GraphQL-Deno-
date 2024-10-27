ffba544491481c3b02da88de0974bf5e832a11c5
sameerhamid <sameerhamid213@gmail.com>
2024-10-27 12:05:05 +0530

    add getCart method to display cart items

--- 12_no-sql/controllers/shop.js
+++ 12_no-sql/controllers/shop.js
@@ -46,47 +46,18 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  // Cart.getProducts((cartProducts) => {
-  //   const cart = [];
-  //   Product.fetchAll((products) => {
-  //     for (prod of products) {
-  //       const cartProductDAta = cartProducts.products.find(
-  //         (p) => p.id === prod.id
-  //       );
-  //       if (cartProductDAta) {
-  //         cart.push({ productData: prod, qty: cartProductDAta.qty });
-  //       }
-  //     }
-
-  //     console.log("cartProductDAta>>>", cart);
-  //     res.render("shop/cart", {
-  //       path: "/cart",
-  //       pageTitle: "Your Cart",
-  //       products: cart,
-  //     });
-  //   });
-  // });
-
   req.user
     .getCart()
-    .then((cart) => {
-      return cart
-        .getProducts()
-        .then((products) => {
-          res.render("shop/cart", {
-            path: "/cart",
-            pageTitle: "Your Cart",
-            products: products,
-          });
-        })
-        .catch((error) => {
-          console.log("Error fetching products from cart: ", error);
-          // res.redirect("/");
-        });
+    .then((products) => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+      });
     })
     .catch((error) => {
-      console.log("Error fetching user's cart: ", error);
-      // res.redirect("/");
+      console.log("Error fetching products from cart: ", error);
+      res.redirect("/");
     });
 };
 
@@ -99,39 +70,12 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then((result) => {
-      console.log("result>>>", result);
+      res.redirect("/cart");
+    })
+    .catch((error) => {
+      console.log("Erorr adding to cart>>>", error);
+      res.redirect("/");
     });
-  // req.user
-  //   .getCart()
-  //   .then((cart) => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } }).then((products) => {
-  //       let product;
-  //       if (products.length > 0) {
-  //         product = products[0];
-  //       }
-
-  //       if (product) {
-  //         const oldQuantity = product.cartItem.quantity;
-  //         newQuantity = oldQuantity + 1;
-  //         return product;
-  //       }
-
-  //       return Product.findByPk(prodId);
-  //     });
-  //   })
-  //   .then((product) => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity },
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect("/cart");
-  //   })
-  //   .catch((err) => {
-  //     console.log("Error fetching user's cart: ", err);
-  //     return res.redirect("/");
-  //   });
 };
 
 exports.postCartDelete = (req, res, next) => {
--- 12_no-sql/models/user.js
+++ 12_no-sql/models/user.js
@@ -42,6 +42,29 @@ class User {
     );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((item) => item.productId);
+    return db
+      .collection("products")
+      .find({
+        _id: {
+          $in: productIds,
+        },
+      })
+      .toArray()
+      .then((products) => {
+        return products.map((product) => {
+          return {
+            ...product,
+            quantity: this.cart.items.find(
+              (i) => i.productId.toString() === product._id.toString()
+            ).quantity,
+          };
+        });
+      });
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
