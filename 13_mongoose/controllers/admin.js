cc7d0bcff8ff440e76101af5b2dccbe8e6013361
sameerhamid <sameerhamid213@gmail.com>
2024-10-28 21:23:51 +0530

    add logic for delete product

--- 13_mongoose/controllers/admin.js
+++ 13_mongoose/controllers/admin.js
@@ -78,7 +78,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const { productId } = req.body;
 
-  Product.deleteById(productId)
+  Product.findByIdAndDelete(productId)
     .then((result) => {
       console.log("Product deleted successfully");
       return res.redirect("/admin/products");
