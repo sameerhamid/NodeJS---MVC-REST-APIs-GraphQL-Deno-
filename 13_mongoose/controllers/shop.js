e72e2c1e79b772fab9fa0960d2dc059b57aa983f
sameerhamid <sameerhamid213@gmail.com>
2024-10-28 21:10:04 +0530

    get products from database use find method of monggose:

--- 13_mongoose/controllers/shop.js
+++ 13_mongoose/controllers/shop.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
   // retrieves all products from the table
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
@@ -32,7 +32,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("shop/index", {
         prods: products,
