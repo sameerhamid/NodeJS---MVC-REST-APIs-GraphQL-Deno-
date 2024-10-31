00082f7f90f870621dc460866e2fb51d7922c5b4
sameerhamid <sameerhamid213@gmail.com>
2024-10-31 07:56:51 +0530

    add user model
    add middleware for user the get User in any request

--- 13_mongoose/app.js
+++ 13_mongoose/app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -21,18 +21,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // adding middleware to use it anywere to retereve the user
 
-// app.use((req, res, next) => {
-//   User.findById("671de86f544086be3660ca27")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => {
-//       console.log("Error fetching user>>>", err);
-//       req.user = null;
-//       next();
-//     });
-// });
+app.use((req, res, next) => {
+  User.findById("671de86f544086be3660ca27")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log("Error fetching user>>>", err);
+      req.user = null;
+      next();
+    });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -44,6 +44,18 @@ mongoose
     "mongodb+srv://codewithsamiir:NSXtwPvAKpa1RFra@cluster0.zfjhc.mongodb.net/shop-mongoose?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then((result) => {
+    // this method return the first object from the database
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Test",
+          email: "test@gmail.com",
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
+
     app.listen(3000);
     console.log("Connected to MongoDB!");
     console.log("Server running on port 3000");
