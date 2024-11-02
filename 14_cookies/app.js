a59aeb2b76768835b96c6fc4ee2e0d515f6b3972
sameerhamid <sameerhamid213@gmail.com>
2024-11-02 09:30:12 +0530

    add auth routes there login route and login controller
    add auth login ejs page

--- 14_cookies/app.js
+++ 14_cookies/app.js
@@ -1,56 +1,66 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
+  User.findById("5bab316ce0a7c75f783cb8a8")
+    .then((user) => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
+const mongoDbUrl =
+  "mongodb+srv://codewithsamiir:NSXtwPvAKpa1RFra@cluster0.zfjhc.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0";
+
 mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
-  .then(result => {
-    User.findOne().then(user => {
+  .connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: {
+      w: "majority",
+      wtimeout: 5000,
+    },
+  })
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: "test",
+          email: "test@gmail.com",
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
     });
-    app.listen(3000);
+    app.listen(3000, () => console.log("Server is running on port 3000"));
   })
-  .catch(err => {
-    console.log(err);
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
   });
---
+++ 14_cookies/routes/auth.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const express = require("express");
+const authControllers = require("../controllers/auth");
+const router = express.Router();
+
+router.get("/login", authControllers.getLogin);
+
+module.exports = router;
