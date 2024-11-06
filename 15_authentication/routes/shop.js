abd27f56efef5d7b6114c144f7c0479d767c891f
sameerhamid <sameerhamid213@gmail.com>
2024-11-06 21:50:01 +0530

    add middleware for isAuthenticated and use it for protectd routes

--- 15_authentication/routes/shop.js
+++ 15_authentication/routes/shop.js
@@ -6,19 +6,21 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
+const isAuth = require("../middleware/is-auth");
+
 router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
 router.get("/products/:productId", shopController.getProduct);
 
-router.get("/cart", shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
-router.post("/cart", shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.get("/orders", shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
-router.post("/create-order", shopController.postOrders);
+router.post("/create-order", isAuth, shopController.postOrders);
 
-router.post("/cart-delete-item", shopController.postCartDelete);
+router.post("/cart-delete-item", isAuth, shopController.postCartDelete);
 module.exports = router;
