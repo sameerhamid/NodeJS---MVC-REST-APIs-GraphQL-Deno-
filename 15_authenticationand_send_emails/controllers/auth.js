7824dc5bd30756d55e3f251c778645ca5d420474
sameerhamid <sameerhamid213@gmail.com>
2024-11-09 09:18:22 +0530

    add new password page and routes and controllers

--- 15_authenticationand_send_emails/controllers/auth.js
+++ 15_authenticationand_send_emails/controllers/auth.js
@@ -192,3 +192,62 @@ exports.postReset = (req, res, next) => {
       });
   });
 };
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        pageTitle: "New Password",
+        path: "/new-password",
+        errorMsg: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((error) => {
+      console.log("Error while finding user>>>", error);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const { userId, password, confirmPassword, passwordToken } = req.body;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "No user found.");
+        return res.redirect("/reset");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPass) => {
+          user.password = hashedPass;
+          user.resetToken = undefined;
+          user.resetTokenExpiration = undefined;
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log("Error while updating password>>>", err);
+        });
+    })
+    .catch((error) => {
+      console.log("Error while finding user>>>", error);
+    });
+};
--- 15_authenticationand_send_emails/routes/auth.js
+++ 15_authenticationand_send_emails/routes/auth.js
@@ -8,10 +8,12 @@ router.post("/login", authControllers.postLogin);
 router.post("/logout", authControllers.postLogout);
 
 router.get("/signup", authControllers.getSignup);
-
 router.post("/signup", authControllers.postSignup);
-router.get("/reset", authControllers.getReset);
 
+router.get("/reset", authControllers.getReset);
 router.post("/reset", authControllers.postReset);
+
+router.get("/reset/:token", authControllers.getNewPassword);
+router.post("/new-password", authControllers.postNewPassword);
 
 module.exports = router;
