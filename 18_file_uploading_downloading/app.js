89947d3c43a65d02e87e7a55abeff2a3a344e299
sameerhamid <sameerhamid213@gmail.com>
2024-11-12 21:32:05 +0530

    add middlware for images path
    add / before all image rendering

--- 18_file_uploading_downloading/app.js
+++ 18_file_uploading_downloading/app.js
@@ -68,6 +68,8 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "public")));
+// serve images from /images folder
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
   session({
     secret: "my secret",
