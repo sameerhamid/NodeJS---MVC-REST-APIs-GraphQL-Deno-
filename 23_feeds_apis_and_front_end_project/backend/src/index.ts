9ef2e04a8f0d19c5540e5864f3a8fdf715beb34d
sameerhamid <sameerhamid213@gmail.com>
2024-11-19 21:07:52 +0530

    add feeds prject with frontend and backend setup

---
+++ 23_feeds_apis_and_front_end_project/backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import feedRoutes from "./routes/feed.route";
+
+const app = express();
+// app.use(bodyParser.urlencoded({ extended: false })); //x-www-form-urlencoded
+
+app.use(bodyParser.json()); // application/json
+
+/**
+ * Middleware to set CORS headers for all incoming requests
+ * @function
+ * @param {Request} _req - Express request object (unused)
+ * @param {Response} res - Express response object
+ * @param {NextFunction} next - Express next middleware function
+ */
+app.use((_req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST , PATCH, DELETE , PUT"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+//--------------- GET /feed/posts ----------------
+app.use("/feed", feedRoutes);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
