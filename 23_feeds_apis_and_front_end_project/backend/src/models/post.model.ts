d5a2fea19c71f5c308052853bd362fcfa118ac81
sameerhamid <sameerhamid213@gmail.com>
2024-11-19 22:11:09 +0530

    add commenting to post model

--- 23_feeds_apis_and_front_end_project/backend/src/models/post.model.ts
+++ 23_feeds_apis_and_front_end_project/backend/src/models/post.model.ts
@@ -1,29 +1,37 @@
 import mongoose, { Schema } from "mongoose";
 
+// Define the schema for a post
 const postSchema = new Schema(
   {
+    // Title of the post
     title: {
       type: String,
-      required: true,
+      required: true, // Title is required
     },
+    // Content of the post
     content: {
       type: String,
-      required: true,
+      required: true, // Content is required
     },
+    // URL of the image associated with the post
     imageUrl: {
       type: String,
-      required: true,
+      required: true, // Image URL is required
     },
+    // Creator of the post
     creator: {
-      type: Object,
+      type: Object, // Creator is an object containing creator details
+      // Uncomment the lines below if referencing a User model
       // type: Schema.Types.ObjectId,
       // ref: "User",
-      required: true,
+      required: true, // Creator is required
     },
   },
-  { timestamps: true }
+  { timestamps: true } // Automatically add createdAt and updatedAt timestamps
 );
 
+// Create a mongoose model for posts using the defined schema
 const Post = mongoose.model("Post", postSchema);
 
+// Export the Post model for use in other parts of the application
 export default Post;
