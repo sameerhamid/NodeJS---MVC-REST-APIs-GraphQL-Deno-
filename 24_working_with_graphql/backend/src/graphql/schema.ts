b8834adbf50c39a2790de62e5c0b8a659a88f1cf
sameerhamid <sameerhamid213@gmail.com>
2024-11-25 07:59:26 +0530

    fix typos in schema types

--- 24_working_with_graphql/backend/src/graphql/schema.ts
+++ 24_working_with_graphql/backend/src/graphql/schema.ts
@@ -1,42 +1,43 @@
 import { buildSchema } from "graphql";
 
 const Schema = buildSchema(`
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String! 
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
+  }
 
-    type Post{
-    _id:ID!
-    title:String!
-    content:String!
-    iamgeUrl:String!
-    creator:User!
-    createdAt:String!
-    updatedAt:String!
-    }
-
-    type User{
-    _id:ID!
+  type User {
+    _id: ID!
     email: String!
     password: String
     name: String!
-    status:String!
-    post:[Post!]!
-    }
+    status: String!
+    posts: [Post!]! 
+  }
 
-    input UserInputData{
-        email: String!
-        password: String!
-        name: String!
-    }
-    type RootMutation{
-        createUser(userInput:UserInputData!):User!
-    }
-  
-    type RootQuery{
-        hello:String
-    }
-   schema{
-   query:RootQuery
-   mutation: RootMutation
-   }
-    `);
+  input UserInputData {
+    email: String!
+    password: String!
+    name: String!
+  }
+
+  type RootMutation {
+    createUser(userInput: UserInputData!): User!
+  }
+
+  type RootQuery {
+    hello: String
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`);
 
 export default Schema;
