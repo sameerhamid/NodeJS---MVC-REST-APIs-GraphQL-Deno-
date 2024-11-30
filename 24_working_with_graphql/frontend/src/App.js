2154e20ce6801f65eebde3d9d5be16e2bba42bca
sameerhamid <sameerhamid213@gmail.com>
2024-11-30 12:11:03 +0530

    add login query in front end

--- 24_working_with_graphql/frontend/src/App.js
+++ 24_working_with_graphql/frontend/src/App.js
@@ -56,55 +56,55 @@ class App extends Component {
     localStorage.removeItem("userId");
   };
 
-  loginHandler = (event, authData) => {
-    event.preventDefault();
-    this.setState({ authLoading: true });
-    fetch("http://localhost:8080/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password,
-      }),
-    })
-      .then((res) => {
-        if (res.status === 422) {
-          throw new Error("Validation failed.");
-        }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log("Error!");
-          throw new Error("Could not authenticate you!");
-        }
-        return res.json();
-      })
-      .then((resData) => {
-        console.log(resData);
-        this.setState({
-          isAuth: true,
-          token: resData.token,
-          authLoading: false,
-          userId: resData.userId,
-        });
-        localStorage.setItem("token", resData.token);
-        localStorage.setItem("userId", resData.userId);
-        const remainingMilliseconds = 60 * 60 * 1000;
-        const expiryDate = new Date(
-          new Date().getTime() + remainingMilliseconds
-        );
-        localStorage.setItem("expiryDate", expiryDate.toISOString());
-        this.setAutoLogout(remainingMilliseconds);
-      })
-      .catch((err) => {
-        console.log("error>>>", err);
-        this.setState({
-          isAuth: false,
-          authLoading: false,
-          error: err,
-        });
-      });
-  };
+  // loginHandler = (event, authData) => {
+  //   event.preventDefault();
+  //   this.setState({ authLoading: true });
+  //   fetch("http://localhost:8080/auth/login", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       email: authData.email,
+  //       password: authData.password,
+  //     }),
+  //   })
+  //     .then((res) => {
+  //       if (res.status === 422) {
+  //         throw new Error("Validation failed.");
+  //       }
+  //       if (res.status !== 200 && res.status !== 201) {
+  //         console.log("Error!");
+  //         throw new Error("Could not authenticate you!");
+  //       }
+  //       return res.json();
+  //     })
+  //     .then((resData) => {
+  //       console.log(resData);
+  //       this.setState({
+  //         isAuth: true,
+  //         token: resData.token,
+  //         authLoading: false,
+  //         userId: resData.userId,
+  //       });
+  //       localStorage.setItem("token", resData.token);
+  //       localStorage.setItem("userId", resData.userId);
+  //       const remainingMilliseconds = 60 * 60 * 1000;
+  //       const expiryDate = new Date(
+  //         new Date().getTime() + remainingMilliseconds
+  //       );
+  //       localStorage.setItem("expiryDate", expiryDate.toISOString());
+  //       this.setAutoLogout(remainingMilliseconds);
+  //     })
+  //     .catch((err) => {
+  //       console.log("error>>>", err);
+  //       this.setState({
+  //         isAuth: false,
+  //         authLoading: false,
+  //         error: err,
+  //       });
+  //     });
+  // };
 
   // signupHandler = (event, authData) => {
   //   event.preventDefault();
@@ -146,6 +146,69 @@ class App extends Component {
   //       });
   //     });
   // };
+
+  loginHandler = (event, authData) => {
+    event.preventDefault();
+    this.setState({ authLoading: true });
+    const graphqlQuery = `
+    query{
+  login(email:"${authData.email}",password:"${authData.password}"){
+		token
+    userId
+  }
+}
+    `;
+    fetch("http://localhost:8080/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: graphqlQuery,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((resData) => {
+        if (
+          resData.errors &&
+          resData.errors.length > 0 &&
+          resData.errors[0].status === 422
+        ) {
+          throw new Error(
+            "Validation failed. Make sure the email address isn't used yet!"
+          );
+        }
+
+        if (resData.errors) {
+          throw new Error("User Login failed!");
+        }
+        console.log(resData);
+        this.setState({
+          isAuth: true,
+          token: resData.data.login.token,
+          authLoading: false,
+          userId: resData.data.login.userId,
+        });
+        localStorage.setItem("token", resData.data.login.token);
+        localStorage.setItem("userId", resData.data.login.userId);
+        const remainingMilliseconds = 60 * 60 * 1000;
+        const expiryDate = new Date(
+          new Date().getTime() + remainingMilliseconds
+        );
+        localStorage.setItem("expiryDate", expiryDate.toISOString());
+        this.setAutoLogout(remainingMilliseconds);
+      })
+      .catch((err) => {
+        console.log("error>>>", err);
+        this.setState({
+          isAuth: false,
+          authLoading: false,
+          error: err,
+        });
+      });
+  };
 
   signupHandler = (event, authData) => {
     event.preventDefault();
