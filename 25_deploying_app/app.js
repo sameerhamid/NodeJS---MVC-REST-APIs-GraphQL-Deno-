b9b035f2dda57ffc762a914ff3cd5db00fc263cf
sameerhamid <sameerhamid213@gmail.com>
2024-12-08 11:57:21 +0530

    add compression middlware

--- 25_deploying_app/app.js
+++ 25_deploying_app/app.js
@@ -10,6 +10,7 @@ const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
 const helmet = require("helmet");
+const compression = require("compression");
 
 const errorController = require("./controllers/error");
 
@@ -62,6 +63,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 app.use(helmet());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
 // Initialize multer middleware
 app.use(
